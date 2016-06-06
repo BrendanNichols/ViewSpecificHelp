b0839853184d9b3c2c512d7564dc2e0ade418870
Brendan Nichols <BrendanNichols@beckgroup.com>
2016-06-06 13:33:35 -0400

    init commit. Troll link

---
+++ ViewSpecificHelp/Entry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace ViewSpecificHelp
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Entry:IExternalApplication
+    {
+        static string _path = typeof(Entry).Assembly.Location;
+
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication app)
+        {
+            RibbonPanel Room3d = app.CreateRibbonPanel("Beck", "Revit Help");
+            PushButtonData buttonData = new PushButtonData("myButton", "Revit Help", _path, "ViewSpecificHelp.Main");
+
+            Uri uriImage = new Uri("////Atlfs//public//BNichols//Coding//ViewSpecificHelp//QuestionMarkSmall.png");
+            BitmapImage largeImage = new BitmapImage(uriImage);
+
+            buttonData.LargeImage = largeImage;
+
+            RibbonItem myButton = Room3d.AddItem(buttonData);
+
+
+
+            return Result.Succeeded;
+
+        }
+    }
+}
---
+++ ViewSpecificHelp/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB.Structure;
+
+
+namespace ViewSpecificHelp
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            GetViewInfo(commandData.Application.ActiveUIDocument);
+            return Result.Succeeded;
+        }
+
+
+        public void GetViewInfo (UIDocument uiDoc)
+        {
+            string viewTitle = uiDoc.ActiveView.Title;
+            ViewRulesTester(viewTitle);
+        }
+
+        public void ViewRulesTester (string viewTitle)
+        {
+            ProcessStartInfo google = new ProcessStartInfo(@"http://lmgtfy.com/?q=How+to+Revit%3F");
+            Process.Start(google);
+        }
+    }
+}
